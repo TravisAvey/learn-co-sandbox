cac18819f09be95ef71a7e9ff9bb5bb81586011a
Travis Avey <avey.travis@gmail.com>
2018-07-18 13:34:31 +0000

    automatically backed up by learn

---
+++ test.js
@@ -0,0 +1,9 @@
+var dog = "thirsty";
+
+if (dog === "hungry") {
+  console.log("Need more food!");
+} else if (dog === "thirsty") {
+  console.log("Need more waters");
+} else {
+  console.log("Dog is happy!");
+}
